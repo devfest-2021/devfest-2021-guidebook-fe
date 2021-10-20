fd9cbac869e99dd318e3885e85f32a5b1c89b5c3
HYEOK_E <jhjeong00@gmail.com>
2021-10-20 21:00:47 +0900

    chore : app.js 라우터 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
-import Login from './Pages/Login';
-import Register from './Pages/Register';
-import Navigation from './Components/Navigation';
-import Section from './Pages/Section';
 import './App.css';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import { SWRConfig } from 'swr';
-import { Sample } from './components/sample';
+import { Sample } from './components/sample/index';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <SWRConfig>
         <Sample />
-       <Navigation />
-      <Switch>
-        <Route exact path="/" component={Section} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/section" component={Section} />
-      </Switch>
       </SWRConfig>
     </ThemeProvider>
 
