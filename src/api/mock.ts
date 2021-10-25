8368cb923626f9d659aeedd0c9294ab062330c77
leeyun1533 <leeyun1533@naver.com>
2021-10-25 21:54:45 +0900

    fix: fix the misc

--- src/api/mock.ts
+++ src/api/mock.ts
@@ -1,118 +1,118 @@
 export const sessionList = [
   {
-    id: 1,
+    session_id: 1,
     title: '어쩌다 ,,, 가 되었나',
     organizer: '고려대학교',
     description:
       '어쩌다 진로방향을 잡게되었는지, 혹은 의도하지 않았지만 어쩌다보니 이 일을 하게 되었다.. 그냥 지금 하고 있는 자리에 있게 된 배경을 들어보자.',
-    startAt: +new Date('2021-10-11'),
+    start_at: +new Date('2021-10-11'),
     endAt: +new Date('2021-12-11'),
     runningTime: +new Date('2021-12-12'),
     logoImgUrl: 'https://picsum.photos/200',
   },
   {
-    id: 2,
+    session_id: 2,
     title: '어쩌다 ,,, 가 되었나',
     organizer: '고려대학교',
     description:
       '어쩌다 진로방향을 잡게되었는지, 혹은 의도하지 않았지만 어쩌다보니 이 일을 하게 되었다.. 그냥 지금 하고 있는 자리에 있게 된 배경을 들어보자.',
-    startAt: +new Date('2021-10-11'),
+    start_at: +new Date('2021-10-11'),
     endAt: +new Date('2021-12-11'),
     runningTime: +new Date('2021-12-12'),
     logoImgUrl: 'https://picsum.photos/200',
   },
   {
-    id: 3,
+    session_id: 3,
     title: '어쩌다 ,,, 가 되었나',
     organizer: '고려대학교',
 
     description:
       '어쩌다 진로방향을 잡게되었는지, 혹은 의도하지 않았지만 어쩌다보니 이 일을 하게 되었다.. 그냥 지금 하고 있는 자리에 있게 된 배경을 들어보자.',
-    startAt: +new Date('2021-10-11'),
+    start_at: +new Date('2021-10-11'),
     endAt: +new Date('2021-12-11'),
     runningTime: +new Date('2021-12-12'),
     logoImgUrl: 'https://picsum.photos/200',
   },
   {
-    id: 4,
+    session_id: 4,
     title: '어쩌다 ,,, 가 되었나',
     organizer: '고려대학교',
 
     description:
       '어쩌다 진로방향을 잡게되었는지, 혹은 의도하지 않았지만 어쩌다보니 이 일을 하게 되었다.. 그냥 지금 하고 있는 자리에 있게 된 배경을 들어보자.',
-    startAt: +new Date('2021-10-11'),
+    start_at: +new Date('2021-10-11'),
     endAt: +new Date('2021-12-11'),
     runningTime: +new Date('2021-12-12'),
     logoImgUrl: 'https://picsum.photos/200',
   },
   {
-    id: 5,
+    session_id: 5,
     title: '어쩌다 ,,, 가 되었나',
     organizer: '고려대학교',
 
     description:
       '어쩌다 진로방향을 잡게되었는지, 혹은 의도하지 않았지만 어쩌다보니 이 일을 하게 되었다.. 그냥 지금 하고 있는 자리에 있게 된 배경을 들어보자.',
-    startAt: +new Date('2021-10-11'),
+    start_at: +new Date('2021-10-11'),
     endAt: +new Date('2021-12-11'),
     runningTime: +new Date('2021-12-12'),
     logoImgUrl: 'https://picsum.photos/200',
   },
   {
-    id: 6,
+    session_id: 6,
     title: '어쩌다 ,,, 가 되었나',
     organizer: '고려대학교',
 
     description:
       '어쩌다 진로방향을 잡게되었는지, 혹은 의도하지 않았지만 어쩌다보니 이 일을 하게 되었다.. 그냥 지금 하고 있는 자리에 있게 된 배경을 들어보자.',
-    startAt: +new Date('2021-10-11'),
+    start_at: +new Date('2021-10-11'),
     endAt: +new Date('2021-12-11'),
     runningTime: +new Date('2021-12-12'),
     logoImgUrl: 'https://picsum.photos/200',
   },
   {
-    id: 7,
+    session_id: 7,
     title: '어쩌다 ,,, 가 되었나',
     organizer: '고려대학교',
 
     description:
       '어쩌다 진로방향을 잡게되었는지, 혹은 의도하지 않았지만 어쩌다보니 이 일을 하게 되었다.. 그냥 지금 하고 있는 자리에 있게 된 배경을 들어보자.',
-    startAt: +new Date('2021-10-11'),
+    start_at: +new Date('2021-10-11'),
     endAt: +new Date('2021-12-11'),
     runningTime: +new Date('2021-12-12'),
     logoImgUrl: 'https://picsum.photos/200',
   },
   {
-    id: 8,
+    session_id: 8,
     title: '어쩌다 ,,, 가 되었나',
     organizer: '고려대학교',
 
     description:
       '어쩌다 진로방향을 잡게되었는지, 혹은 의도하지 않았지만 어쩌다보니 이 일을 하게 되었다.. 그냥 지금 하고 있는 자리에 있게 된 배경을 들어보자.',
-    startAt: +new Date('2021-10-11'),
+    start_at: +new Date('2021-10-11'),
     endAt: +new Date('2021-12-11'),
     runningTime: +new Date('2021-12-12'),
     logoImgUrl: 'https://picsum.photos/200',
   },
   {
-    id: 9,
+    session_id: 9,
     title: '어쩌다 ,,, 가 되었나',
     organizer: '고려대학교',
 
     description:
       '어쩌다 진로방향을 잡게되었는지, 혹은 의도하지 않았지만 어쩌다보니 이 일을 하게 되었다.. 그냥 지금 하고 있는 자리에 있게 된 배경을 들어보자.',
-    startAt: +new Date('2021-10-11'),
+    start_at: +new Date('2021-10-11'),
     endAt: +new Date('2021-12-11'),
     runningTime: +new Date('2021-12-12'),
     logoImgUrl: 'https://picsum.photos/200',
   },
   {
-    id: 10,
+    session_id: 10,
     title: '어쩌다 ,,, 가 되었나',
     organizer: '고려대학교',
 
     description:
       '어쩌다 진로방향을 잡게되었는지, 혹은 의도하지 않았지만 어쩌다보니 이 일을 하게 되었다.. 그냥 지금 하고 있는 자리에 있게 된 배경을 들어보자.',
-    startAt: +new Date('2021-10-11'),
+    start_at: +new Date('2021-10-11'),
     endAt: +new Date('2021-12-11'),
     runningTime: +new Date('2021-12-12'),
     logoImgUrl: 'https://picsum.photos/200',
