f85746fb32bfea92f51024ead5a2ce9f07058f52
HYEOK_E <jhjeong00@gmail.com>
2021-10-27 21:28:21 +0900

    feat : nav sticky 속성으로 변경C

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router';
 import { Guestbook } from './Guestbook';
 import { Session } from './Session';
+import Navigation from '../common/navigation';
 
 export const Main = () => {
   return (
