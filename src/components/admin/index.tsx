ef951d1b0bb4d477dddf7fa800c5006ecb06d239
gomjellie <gomjellie@gmail.com>
2021-10-27 22:59:22 +0900

    feat: 출석 필터 (출석일, 이메일, 소속) 추가

--- src/components/admin/index.tsx
+++ src/components/admin/index.tsx
@@ -1,7 +1,72 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { LayoutContainer } from 'src/styles/layout';
+import { Slider, Input, Lookup } from 'react-rainbow-components';
+import { FilterRow, FormContainer, FormLabel, FullWidth } from './styled';
+import { LookupValue } from 'react-rainbow-components/components/types';
+
+const SchoolList = [{ label: '숭실대' }, { label: '서울대' }];
+
 const Admin: FC = () => {
-  return <LayoutContainer>Admin</LayoutContainer>;
+  const [attendanceCount, setAttendanceCount] = useState(0);
+  const [email, setEmail] = useState<string>('');
+  const onAttendanceChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setAttendanceCount(Number(event.target.value));
+  const onEmailChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setEmail(event.target.value);
+  const [school, setSchool] = useState<{
+    options: LookupValue[];
+    value: string;
+  }>({
+    options: [],
+    value: '',
+  });
+  const onSearchSchool = (value: string) => {
+    const options = SchoolList.filter((item) => item.label.includes(value));
+    setSchool({ options, value });
+  };
+
+  useEffect(() => {
+    console.log(email, school.value);
+  });
+
+  return (
+    <LayoutContainer>
+      <h1>출석필터</h1>
+      <FilterRow>
+        <FormContainer>
+          <FormLabel>출석일</FormLabel>
+          <Slider
+            value={attendanceCount}
+            min={1}
+            max={14}
+            style={FullWidth}
+            onChange={onAttendanceChange}
+          />
+        </FormContainer>
+        <FormContainer>
+          <FormLabel>이메일</FormLabel>
+          <Input
+            placeholder="example@gmail.com"
+            style={FullWidth}
+            onChange={onEmailChange}
+          />
+        </FormContainer>
+        <FormContainer>
+          <FormLabel>소속</FormLabel>
+          <Lookup
+            placeholder="숭실대"
+            style={FullWidth}
+            options={school.options}
+            value={school.options.find((item) => item.label === school.value)}
+            onChange={(value) =>
+              setSchool({ ...school, value: value?.label?.toString() ?? '' })
+            }
+            onSearch={onSearchSchool}
+          />
+        </FormContainer>
+      </FilterRow>
+    </LayoutContainer>
+  );
 };
 
 export default Admin;
--- src/components/common/navigation/index.tsx
+++ src/components/common/navigation/index.tsx
@@ -29,7 +29,6 @@ const Navigation: React.FC = () => {
           </NavTask>
         </NavTaskWrapper>
         <NavTaskWrapper>
-
           {user.email.length > 5 && user.nickname.length > 1 ? (
             <NavTask
               onClick={() => {
@@ -62,7 +61,6 @@ const Navigation: React.FC = () => {
               프로필 만들기
             </OpenedModal>
           </NavTask> */}
-
         </NavTaskWrapper>
       </NavInner>
     </NavDesign>
