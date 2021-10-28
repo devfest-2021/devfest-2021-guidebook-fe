409d85ee70ddca116ba2508946722c4e120a1a74
HYEOK_E <jhjeong00@gmail.com>
2021-10-28 15:45:55 +0900

    chore : 갤럭시 폴드 화면 width 수정

--- src/components/common/Modal/styled.ts
+++ src/components/common/Modal/styled.ts
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 import { Modal } from 'react-rainbow-components';
 
 export const StyledModal = styled(Modal)`
-  min-width: 300px;
+  min-width: 250px;
   max-width: 450px;
+  padding: 10px;
+  @media (max-width: 280px) {
+    padding: 0px;
+  }
 `;
