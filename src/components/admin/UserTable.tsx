e32f774fa4d699edf84ed69f9cf73f50dd9b5562
gomjellie <gomjellie@gmail.com>
2021-11-05 14:25:46 +0900

    feat: 코드리뷰반영.  참석수 필터 ===으로  변경, early return으로 코드 간결하게 리팩토링

--- src/components/admin/UserTable.tsx
+++ src/components/admin/UserTable.tsx
@@ -3,6 +3,7 @@ import { Table, Column } from 'react-rainbow-components';
 import { useRecoilValue } from 'recoil';
 import { useAdminUserList } from 'src/api/hooks/useAdmin';
 import { adminState } from 'src/store/admin';
+import inko from 'src/utils/inko';
 
 export type AdminUser = {
   user_id: number;
@@ -44,16 +45,21 @@ const UserTable: FC = () => {
     const sorter = reduceSorter(sortBy, sortDirection);
     const filteredUsers = userList
       .filter((user) => {
-        if (school.value !== '') {
-          if (user.group.includes(school.value)) {
-            return true;
-          }
-          return false;
+        if (school.value === '') return true;
+
+        if (
+          inko
+            .ko2en(user.group)
+            .toLowerCase()
+            .includes(inko.ko2en(school.value).toLowerCase())
+        ) {
+          return true;
         }
-        return true;
+        return false;
       })
       .filter((user) => {
-        if (attendanceCount <= user.count) {
+        if (attendanceCount === 0) return true;
+        if (attendanceCount === user.count) {
           return true;
         }
         return false;
@@ -88,7 +94,6 @@ const UserTable: FC = () => {
       sortDirection={sortDirection}
       sortedBy={sortBy}
       style={{ height: 'auto' }}
-      isLoading={users.length === 0}
     >
       <Column header="참석수" field="count" width={120} sortable />
       <Column header="user_id" field="user_id" />
