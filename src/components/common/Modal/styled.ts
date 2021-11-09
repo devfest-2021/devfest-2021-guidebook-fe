aeae8d28f111a4c3e2b5738f7fec5a73d8af80c2
HYEOK_E <jhjeong00@gmail.com>
2021-11-09 23:55:02 +0900

    feat : 버튼 링크 block, count block

--- src/components/common/Modal/styled.ts
+++ src/components/common/Modal/styled.ts
@@ -3,9 +3,16 @@ import { Modal } from 'react-rainbow-components';
 
 export const StyledModal = styled(Modal)`
   min-width: 250px;
-  max-width: 450px;
-
-  padding: 10px;
+  max-width: 350px;
+  @media (max-width: 500px) {
+    max-width: 300px;
+  }
+  @media (max-width: 400px) {
+    max-width: 280px;
+  }
+  @media (max-width: 320px) {
+    max-width: 250px;
+  }
   @media (max-width: 280px) {
     padding: 0px;
   }
